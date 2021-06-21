6d70610b3194cef76c9d601e4d4bb18ec6d46217
marcomiretti <marco.miretti@gmail.com>
2021-06-21 16:07:45 -0300

    feat: add systemctl support

--- src/remote_mole/setup/entrypoint.py
+++ src/remote_mole/setup/entrypoint.py
@@ -7,18 +7,26 @@ import os
 from PyInquirer import prompt
 
 
+PROJECT_NAME = 'remote_mole'
+
 AVAILABLE_BOTS = [
     'Discord',
 ]
 
 CONFIG_PATH = os.path.join(
     os.path.expanduser('~'),
-    '.config/remote_mole/config.toml',
+    f'.config/{PROJECT_NAME}/config.toml',
 )
+
+SYSD_SERVICE_PATH = os.path.join(
+    os.path.expanduser('~'),
+    f'.config/systemd/user/{PROJECT_NAME}.service'
+)
+
+PY_EXEC_PATH = '/usr/bin/python3'
 
 
 def _create_file_with_content(filename, config):
-    basename = os.path.basename(filename)
     dirname = os.path.dirname(filename)
     os.mkdir
     try:
@@ -93,11 +101,56 @@ def register():
 
 
 def start():
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read(CONFIG_PATH)
+
+    sysd_file = configparser.ConfigParser()
+    sysd_file.optionxform = str
+
+    this_file = os.path.realpath(__file__)
+    pkg_dir = os.path.dirname(os.path.dirname(this_file))
+
+    if 'Discord' in config.sections():
+        daemon_path = os.path.join(
+            pkg_dir,
+            '_internal/platforms/discord/daemon.py'
+        )
+    else:
+        raise EnvironmentError('No platforms are configured!')
+
+    sysd_file['Unit'] = {
+        'Description': PROJECT_NAME,
+    }
+    sysd_file['Service'] = {
+        'ExecStart': f'{PY_EXEC_PATH} {daemon_path}',
+        'Restart': 'on-failure',
+        'Environment': 'PYTHONUNBUFFERED=1',
+    }
+    sysd_file['Install'] = {
+        'WantedBy': 'default.target',
+    }
+
+    _create_file_with_content(
+        SYSD_SERVICE_PATH,
+        sysd_file,
+    )
+    os.system('systemctl --user daemon-reload')
+
+    os.system('systemctl --user start {PROJECT_NAME}')
+
+
+def stop():
+    os.system('systemctl --user stop {PROJECT_NAME}')
+
+
+def status():
+    os.system('systemctl --user status {PROJECT_NAME}')
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="remote_mole"
+        description=PROJECT_NAME
     )
     parser.add_argument(
         '-register', action='store_true',
@@ -107,6 +160,14 @@ def main():
         '-start', action='store_true',
         help='Start the mole daemon',
     )
+    parser.add_argument(
+        '-stop', action='store_true',
+        help='Stop the mole daemon',
+    )
+    parser.add_argument(
+        '-status', action='store_true',
+        help='Get service status from systemctl',
+    )
     args = parser.parse_args()
 
     if args.register:
@@ -115,6 +176,14 @@ def main():
 
     if args.start:
         start()
+        return 0
+
+    if args.stop:
+        stop()
+        return 0
+
+    if args.status:
+        status()
         return 0
 
 
