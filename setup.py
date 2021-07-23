2b69694e0c0634c038b2ef61eefc6e449c5875c5
marcomiretti <marco.miretti@gmail.com>
2021-07-23 19:55:35 -0300

    chore: pump version after fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="remote_mole",
-    version="1.0.3",
+    version="1.0.4",
     author="Marco Miretti",
     author_email="marcomiretti@gmail.com",
     description=("Create moles on remote setups."),
